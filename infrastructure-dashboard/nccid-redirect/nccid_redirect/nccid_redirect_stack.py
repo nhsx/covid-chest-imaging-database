5f5a0ec09d45928bc58f1970caad4db0e5c7cc35
Gergely Imreh <gergely.imreh@faculty.ai>
2020-12-14 14:48:30 +0000

    use cloudfront to do all these redirections
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

--- infrastructure-dashboard/nccid-redirect/nccid_redirect/nccid_redirect_stack.py
+++ infrastructure-dashboard/nccid-redirect/nccid_redirect/nccid_redirect_stack.py
@@ -1,22 +1,36 @@
-from aws_cdk import (
-    core,
-    aws_lambda as _lambda,
-    aws_apigatewayv2 as _apigw2,
-    aws_apigatewayv2_integrations as _apigw2int,
-    aws_certificatemanager as _acm,
-)
+from aws_cdk import aws_apigatewayv2 as _apigw2
+from aws_cdk import aws_apigatewayv2_integrations as _apigw2int
+from aws_cdk import aws_certificatemanager as _acm
+from aws_cdk import aws_cloudfront as _cloudfront
+from aws_cdk import aws_cloudfront_origins as _origins
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import core
 
 
 class NccidRedirectStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Input variables
+
+        # Domain name to redirect
         domain_name = core.CfnParameter(
             self,
             "domainName",
             type="String",
             description="Domain name to redirect",
         )
+
+        # Here we use a specific certificate from parameter values
+        cert_arn = core.CfnParameter(
+            self,
+            "certArn",
+            type="String",
+            description="Certificate ARN of for the redirection (has to be in us-east-1",
+        )
+        # End: Input variables
+
+        # Infra setup
 
         redirect_fn = _lambda.Function(
             self,
@@ -28,30 +42,8 @@ class NccidRedirectStack(core.Stack):
 
         redirect_integration = _apigw2int.LambdaProxyIntegration(handler=redirect_fn)
 
-        ## Could do our own certificate, if we were to control the DNS settings ourselves.
-        # cert = _acm.Certificate(
-        #     self,
-        #     id="nccid-redirection",
-        #     domain_name=domain_name.value_as_string,
-        #     validation=_acm.CertificateValidation.from_dns(),
-        # )
-
-        # Here we use a specific certificate from paramter values
-        cert_arn = core.CfnParameter(
-            self,
-            "certArn",
-            type="String",
-            description="Certificate ARN of for the redirection",
-        )
         cert = _acm.Certificate.from_certificate_arn(
             self, "cert", cert_arn.value_as_string
-        )
-
-        dn = _apigw2.DomainName(
-            self,
-            "nccid-redirect-domain",
-            domain_name=domain_name.value_as_string,
-            certificate=cert,
         )
 
         http_api = _apigw2.HttpApi(
@@ -59,9 +51,20 @@ class NccidRedirectStack(core.Stack):
             "nccid-redirect",
             api_name="nccid-redirect",
             description="A redirection gateway.",
-            default_domain_mapping={"domain_name": dn},
         )
 
         http_api.add_routes(
             path="/", methods=[_apigw2.HttpMethod.GET], integration=redirect_integration
+        )
+
+        origin_target = http_api.url.replace("https://", "", 1)
+        origin = _origins.HttpOrigin(domain_name=origin_target)
+        behaviour = _cloudfront.BehaviorOptions(origin=origin)
+
+        distribution = _cloudfront.Distribution(  # noqa: F841
+            self,
+            "nccid-redirect-dist",
+            default_behavior=behaviour,
+            certificate=cert,
+            domain_names=[domain_name.value_as_string],
         )
