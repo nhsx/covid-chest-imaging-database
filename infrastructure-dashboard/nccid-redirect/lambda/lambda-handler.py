40e7b68a1247a110349b7a832ee87a017af20b77
Gergely Imreh <gergely.imreh@faculty.ai>
2020-12-14 13:43:59 +0000

    use temporary rather than permanent redirect
    To keep options open
    
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

--- infrastructure-dashboard/nccid-redirect/lambda/lambda-handler.py
+++ infrastructure-dashboard/nccid-redirect/lambda/lambda-handler.py
@@ -1,6 +1,6 @@
 def handler(event, context):
     return {
-        "statusCode": 301,
+        "statusCode": 302,
         "headers": {
             "Location": "https://www.nhsx.nhs.uk/covid-19-response/data-and-covid-19/national-covid-19-chest-imaging-database-nccid/"
         },
