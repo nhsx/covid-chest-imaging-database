e508ccd403e5773a024023667a786699ae7e0373
Gergely Imreh <gergely.imreh@faculty.ai>
2020-12-15 20:48:11 +0000

    update binaries to run
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

--- warehouse-loader/setup.py
+++ warehouse-loader/setup.py
@@ -20,7 +20,11 @@ setup(
     packages=find_packages(),
     classifiers=["Programming Language :: Python :: 3"],
     python_requires=">=3.6",
-    scripts=["bin/warehouseloader", "bin/submittingcentres"],
+    scripts=[
+        "bin/warehouseloader",
+        "bin/submittingcentres",
+        "bin/dataprocess",
+    ],
     install_requires=[
         # pipeline
         "bonobo==0.6.4",
