d5212b35c2b1952eea01f094ed4cf81a64ada77a
Gergely Imreh <gergely.imreh@faculty.ai>
2020-12-16 11:50:46 +0000

    use global s3 client to upload files
    Signed-off-by: Gergely Imreh <gergely.imreh@faculty.ai>

--- warehouse-loader/warehouse/dataprocess.py
+++ warehouse-loader/warehouse/dataprocess.py
@@ -243,8 +243,7 @@ def upload_file(file_name, bucket, object_name=None):
     if object_name is None:
         object_name = file_name
 
-    # Upload the file
-    s3_client = boto3.client("s3")
+    # Upload the file, using the global client
     try:
         s3_client.upload_file(file_name, bucket, object_name)
     except ClientError as e:
