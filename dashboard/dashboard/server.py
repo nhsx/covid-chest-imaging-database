1cf82ed3eb4265da07cda88a1fa6c20b42b4ee75
Gergely Imreh <gergely.imreh@faculty.ai>
2021-02-02 14:40:41 +0000

    formatting

--- dashboard/dashboard/server.py
+++ dashboard/dashboard/server.py
@@ -110,6 +110,6 @@ def create_server():
     @server.errorhandler(404)
     def page_not_found(e):
         # note that we set the 404 status explicitly
-        return render_template('404.html'), 404
+        return render_template("404.html"), 404
 
     return server, oidc
