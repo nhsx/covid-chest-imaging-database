8cd52753fbff9975587199a3766683867c368ccf
Fraser Cook <fraserc6@gmail.com>
2021-11-10 18:46:50 +0000

    Add prefix to local file requests

--- docs/lib/csv.js
+++ docs/lib/csv.js
@@ -1,8 +1,9 @@
 import Papa from 'papaparse'
+import { prefix } from './prefix'
 
 export const loadFile = async (path) => {
    if (!path) return false
-   const response = await fetch(path)
+   const response = await fetch(`${prefix}${path}`)
    const reader = response.body.getReader()
    const decoder = new TextDecoder('utf-8')
    const encoded = await reader.read()
