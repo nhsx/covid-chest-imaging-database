46fd30b2a478c2c1f0653dc7d0a261148cc9e3b0
Fraser Cook <fraserc6@gmail.com>
2021-11-11 09:58:55 +0000

    Add trailing slash to preview build to fix S3 routing issue

--- docs/next.config.js
+++ docs/next.config.js
@@ -4,5 +4,6 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-   pageExtensions: ['js', 'jsx', 'mdx']
+   pageExtensions: ['js', 'jsx', 'mdx'],
+   trailingSlash: process.env.NEXT_PUBLIC_TRAILING_SLASH || false
 })
