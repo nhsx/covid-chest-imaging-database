5218370bd952a8667a6edaa982cec1258e1d34b4
Fraser Cook <fraserc6@gmail.com>
2022-03-30 11:50:55 +0100

    Cleanup experiments page

--- docs/pages/experiments.js
+++ docs/pages/experiments.js
@@ -28,53 +28,6 @@ const StatsMenu = ({ currentNavItem, setCurrentNavItem }) => {
    )
 }
 
-// const Header = ({ currentNavItem, setCurrentNavItem }) => {
-//    return (
-//       <>
-//          {/* Header */}
-//          <header className="py-6 sticky top-0  backdrop-blur-lg z-10">
-//             <Container>
-//                <div className="flex space-x-6 justify-between items-center">
-
-//                   {/* Title */}
-//                   <div className="xl:flex-1 flex justify-start">
-//                      <div>
-//                         <Link href="/">
-//                            <a className="text-white flex items-center">
-//                               <ArrowLeftIcon className="w-4 h-4 hidden sm:inline-block sm:mr-2" />
-//                               <span className="underline">Back to website</span>
-//                            </a>
-//                         </Link>
-//                         <h1 className="text-white text-xl sm:text-2xl font-bold mt-1">NCCID Statistics</h1>
-//                      </div>
-//                   </div>
-
-//                   {/* Nav */}
-//                   <div className="hidden flex-1 lg:flex justify-center">
-//                      <Menu currentNavItem={currentNavItem} setCurrentNavItem={setCurrentNavItem} />
-//                   </div>
-
-//                   {/* Logo */}
-//                   <div className="xl:flex-1 flex justify-end">
-//                      <Link href="/">
-//                         <a>
-//                            <img src="/logo-inverted.svg" alt="NHS UK" className="h-8 sm:h-12" />
-//                         </a>
-//                      </Link>
-//                   </div>
-
-//                </div>
-//             </Container>
-//          </header>
-
-//          {/* Mobile menu */}
-//          <div className="flex justify-center lg:hidden py-3 mb-3">
-//             <Menu currentNavItem={currentNavItem} setCurrentNavItem={setCurrentNavItem} />
-//          </div>
-//       </>
-//    )
-// }
-
 // Define navigation 
 const navigation = [
    { title: 'Operating point', component: <OperatingPoint /> },
